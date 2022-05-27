1338799d11ee158e392714c82407637c0c5e43e1
Daga2001 <dav1dguxman@hotmail.com>
2022-05-27 16:22:15 -0400

    Update 1.0

--- dummy_shell.c
+++ dummy_shell.c
@@ -8,6 +8,7 @@
 
 #define MAX 100
 
+// calculates the commands length.
 int len_cmds(char **chains) {
   for(int i = 0; 1; i++) {
      if(chains[i]==NULL) {
@@ -16,6 +17,7 @@ int len_cmds(char **chains) {
   }
 }
 
+// shows the shell's initialization.
 void init_shell() {
   system("clear");	
   printf("*****************************************************************************\n");
@@ -33,13 +35,16 @@ void init_shell() {
   system("clear");
 }
 
+// defines a type of structure to link a process with its PID.
 typedef struct
 {
   int pid;
   char **command;
 } process;
 
+//runs the main program.
 int main(int argc, char* argv[]) {
+  //defining auxiliar variables
   char **vector_cadenas;
   process background[MAX];
   char cadena[MAX];
@@ -49,6 +54,7 @@ int main(int argc, char* argv[]) {
   
   init_shell();  
 
+  //removes a process from background.
   void delete_from_bg(int pid) {
     for (int c = 0; c < nElemBg; c++){
 	if((int)background[c].pid==pid) {
@@ -64,6 +70,7 @@ int main(int argc, char* argv[]) {
     }
   }
 
+  //stores a process in background.
   void send_to_bg(char **command, int pid) {
     command[commands_len-1] = NULL;
     notIgnoreWait = false;
@@ -71,7 +78,8 @@ int main(int argc, char* argv[]) {
     background[nElemBg] = p; 
     ++nElemBg;
   }
-
+  
+  //reads all processes available in background.
   void read_cmd(char **command) {
     for(int i = 0; 1; i++) {
 	if(command[i+1]==NULL){
@@ -81,7 +89,8 @@ int main(int argc, char* argv[]) {
 	printf("%s ", command[i]);
     }
   }
-
+  
+  //processes simulator
   while (1) {
       pid_t pid;
       printf( "(%d)username@host$ ", getpid());
@@ -89,6 +98,7 @@ int main(int argc, char* argv[]) {
       vector_cadenas = de_cadena_a_vector(cadena);
       commands_len = len_cmds(vector_cadenas); 
       
+      //---------------------COMMANDS--------------------------
       if (strcmp("salir",vector_cadenas[0]) == 0){	
 	 printf("*****************************************************************************\n");
 	 printf("******************** Gracias por usar mi dummy shell ;-) ********************\n");
@@ -118,6 +128,8 @@ int main(int argc, char* argv[]) {
 	}	      
       }
 
+      //-------------------------------------------------------
+
       pid = fork();
       assert(pid >= 0);
       if (pid == 0) { 
@@ -125,22 +137,26 @@ int main(int argc, char* argv[]) {
           //remove & from command
           vector_cadenas[commands_len-1] = NULL;
 	}
+	  //kills child processes
 	  execvp(vector_cadenas[0], vector_cadenas);
           exit(0);
       } else {
         if (strcmp("&", vector_cadenas[commands_len-1])==0){	
 	  printf("command sent to background\n");
-	  usleep(100*1000);
 	  send_to_bg(vector_cadenas, pid);
         }
 	if (notIgnoreWait) {
+	  //waits until child dies
           wait(NULL);
 	}
 	else {
+          //sleeps for executing commands before a child.
 	  usleep(100*1000);
 	}
       }
   }
+
+  //THE END
   printf("*****************************************************************************\n");
   printf("The last command executed was:\n");
   read_cmd(vector_cadenas);
